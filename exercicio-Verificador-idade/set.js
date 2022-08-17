d081dd069e896d13eb2dfdcd51070fe1e741c123
Francisco Maciel <109698009+FranciscoMaciell@users.noreply.github.com>
2022-08-17 14:13:42 -0300

    att exercício js

--- exercicio-Verificador-idade/set.js
+++ exercicio-Verificador-idade/set.js
@@ -1,18 +1,55 @@
 function verificar(){
-    var ano=document.querySelector('input#nasc');
-    var data=Number(ano.value);
-    var masc=document.querySelector('input#masc');
-    var mas=Number(masc.value);
-    var fem=document.querySelector('input#fem');
-    var femi=Number(fem.value);
-    var img=document.querySelector('div#imagem');
+    var data=new Date();
+    var a_atual=data.getFullYear();
+    var a_nasc=document.querySelector('input#nasc');
+    var res=document.querySelector('div#res');
+    var img=document.createElement('img');
 
-    var atual=new Date();
-    var a_atual=atual.getDate();
-    var res=a_atual-data;
-    if(mas&&!femi){
-        img.innertext=`Detectamos um individuo do sexo Masculino de ${res} de idade`;
+    // set atributo id para foto
+    img.setAttribute('id', 'foto')
+
+    if(a_nasc.value.length==0||Number(a_nasc.value)>a_atual){
+        window.alert('[ERRO], digite seus dados novamente');
     }else{
-        img.innertext=`Detectamos um individuo do sexo Feminino de ${res} de idade`;
+        var sex=document.getElementsByName('sexo');
+        var idade=a_atual-Number(a_nasc.value);
+        var genero='';
+
+        if(sex[0].checked){
+            genero='Homem';
+            if(idade>=0&&idade<10){
+                //criança
+                img.setAttribute('src', 'imagens/criancam.png');
+            }else if(idade<21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovemm.png');
+            }else if(idade<50){
+                //adulto
+                img.setAttribute('src', 'imagens/adulto.png');
+            }else{
+                //idoso
+                img.setAttribute('src', 'imagens/idoso.png');
+            }
+        }else if(sex[1].checked){
+            genero='Mulher';
+            if(idade>=0&&idade<10){
+                //criança
+                img.setAttribute('src', 'imagens/criancaf.png');
+            }else if(idade<21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovemf.png');
+            }else if(idade<50){
+                //adulto
+                img.setAttribute('src', 'imagens/adulta.png');
+            }else{
+                //idoso
+                img.setAttribute('src', 'imagens/idosa.png');
+            }
+        }
+        res.style.textAlign='center';
+        res.innerHTML=`Detectamos ${genero} com ${idade} anos.`;
+
+        // ADICIONA UM NOVO ELEMENTO DEPOIS
+        res.appendChild(img)
     }
 }
